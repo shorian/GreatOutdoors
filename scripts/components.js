8ba8c733b192219913157ff533ad95f554b1ebd2
Sid <shorian@users.noreply.github.com>
2025-03-04 15:04:12 -0800

    Navbar converted to web component (#85)
    * Created component for banner.
    
    * Cleaned up some redundant or unnecessary CSS rules, replaced <center> tag surrounding logo with margin in CSS.
    
    * Replaced header with component on all pages.
    
    * Created navbar component. Renamed banner.js to components.js; we' re going to chuck everything in a single file.
    
    * Changed nav items to Home/Destinations/Blog/Content instead of links for each locale. Adjusted nav li styling to make entire button clickable without event listener.
    
    * Replaced navbar with component in all files. Applied auto-formatting.
    
    Use "git diff --ignore-all-space" to view changes, ignoring indentation.
    
    * Updated navbar link location for blog

---
+++ scripts/components.js
@@ -0,0 +1,48 @@
+class Banner extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.setAttribute("style", "display: contents;");
+
+    this.innerHTML = `
+        <header id="banner">
+          <h1>The Great Outdoors: A Poul Nichols Experience</h1>
+      </header>
+      `;
+  }
+}
+
+class NavBar extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.setAttribute("style", "display: contents;");
+
+    this.innerHTML = `
+         <nav id="sidebar">
+        <a href=""><img id="logo" src="images/logo.png" alt="Great Outdoors" /></a>
+        <ul>
+          <li>
+            <a href="index.html">Home</a>
+          </li>
+          <li>
+            <a href="destinationPage.html">Destinations</a>
+          </li>
+          <li>
+            <a href="blogHome.html">Blog</a>
+          </li>
+          <li>
+            <a href="contact.html">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      `;
+  }
+}
+
+customElements.define("banner-component", Banner);
+customElements.define("navbar-component", NavBar);
