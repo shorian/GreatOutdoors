df7cd583b10bf25c9e7c53b656759da6383583df
shorian <shorian@users.noreply.github.com>
2025-03-04 18:44:50 -0800

    Replaced <aside> in RecentPosts with more semantically appropriate <nav>.

--- scripts/components.js
+++ scripts/components.js
@@ -53,15 +53,15 @@ class RecentPosts extends HTMLElement {
     this.setAttribute("style", "display: contents;");
 
     this.innerHTML = `
-    <aside id="recent">
-            <h2>Recent Posts</h2>
+    <nav id="recent" class="cell">
+            <h3>Recent Posts</h3>
     <ul>
         <li><a href="./lake.html">Trip Report: Lake 22</a><br>February 13, , 2025</li>
         <li><a href="">Quieter spots for snowshoeing close to home</a><br> January 27, 2025</li>
         <li><a href="./forest.html">Trip Report: Deer Canyon</a><br> December 19, 2025</li>
         <li><a href="">My new favorite daypack</a><br> November 30, 2025</li>
     </ul>
-    </aside>
+    </nav>
       `;
   }
 }
