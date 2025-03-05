3da8d601438c7f98e729fd10c6673310b3e0d056
shorian <shorian@users.noreply.github.com>
2025-03-04 18:30:56 -0800

    Created RecentPosts element; styles/structure need adjustment

--- scripts/components.js
+++ scripts/components.js
@@ -44,5 +44,28 @@ class NavBar extends HTMLElement {
   }
 }
 
+class RecentPosts extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.setAttribute("style", "display: contents;");
+
+    this.innerHTML = `
+    <aside id="recent">
+            <h2>Recent Posts</h2>
+    <ul>
+        <li><a href="./lake.html">Trip Report: Lake 22</a><br>February 13, , 2025</li>
+        <li><a href="">Quieter spots for snowshoeing close to home</a><br> January 27, 2025</li>
+        <li><a href="./forest.html">Trip Report: Deer Canyon</a><br> December 19, 2025</li>
+        <li><a href="">My new favorite daypack</a><br> November 30, 2025</li>
+    </ul>
+    </aside>
+      `;
+  }
+}
+
 customElements.define("banner-component", Banner);
 customElements.define("navbar-component", NavBar);
+customElements.define("recent-posts", RecentPosts);
