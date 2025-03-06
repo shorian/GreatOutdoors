c11ebf769fc5cf6734c191234cae1f44c0e447f0
shorian <shorian@users.noreply.github.com>
2025-03-05 20:49:34 -0800

    Added temporary page id to comments component. Removed  from components that didn't need it. (where can I find a CSS wizard to teach me the deep lore...)

--- scripts/components.js
+++ scripts/components.js
@@ -50,7 +50,6 @@ class RecentPosts extends HTMLElement {
   }
 
   connectedCallback() {
-    this.setAttribute("style", "display: contents;");
 
     this.innerHTML = `
     <nav id="recent" class="cell">
@@ -72,14 +71,13 @@ class Comments extends HTMLElement {
   }
 
   connectedCallback() {
-    this.setAttribute("style", "display: contents;");
 
     this.innerHTML = `
     <section class="cell">
     <h3>Comments</h3>
         <div
           id="convocomet-widget"
-          data-page=""
+          data-page="falls"
           data-site="71937473052531"
           data-theme="light"
         ></div>
