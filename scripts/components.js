61ab775ec32ccdb255308ed1efb44e6e804664ec
Tsujimoto <45842625+TsujimotoYukasuke@users.noreply.github.com>
2025-03-07 16:17:41 -0800

    Add prettier autoformatting and super-linter
    * Created a workflow that both formats and lints our files
    
    * Removed excessive configuration
    
    * Attempting to fix bug with super-linter being unable to access the default branch
    
    * Checking interaction when not set to act on pull-requests
    
    * Removed DEFAULT_BRANCH to see how super-linter responds
    
    * Attempting fix retrieved from stackoverflow
    
    * Attempting to trick super-linter by giving it the branch that triggered it as a base branch, because it doesn't deserve anything else
    
    * Modified HTML to pass super-linter tests
    
    * Modified CSS to partially pass super-linter tests
    
    * Removed myscripts.js
    
    * Removed JAVASCRIPT_STANDARD linter
    
    * Mangled CSS to satisfy linting rules
    
    * Just one more mangle
    
    * Minor modification; made Prettier and Linter's jobs more descriptive
    
    * Added Prettier config: keep tag brackets on same line, 4-space tabs for JS
    
    * Prettified Code!
    
    * Edited prettier-and-linter to use .prettierrc config
    
    * Update prettier-and-linter.yml
    
    ---------
    
    Co-authored-by: TsujimotoYukasuke <TsujimotoYukasuke@users.noreply.github.com>
    Co-authored-by: shorian <shorian@users.noreply.github.com>

--- scripts/components.js
+++ scripts/components.js
@@ -1,28 +1,28 @@
 class Banner extends HTMLElement {
-  constructor() {
-    super();
-  }
+    constructor() {
+        super();
+    }
 
-  connectedCallback() {
-    this.setAttribute("style", "display: contents;");
+    connectedCallback() {
+        this.setAttribute("style", "display: contents;");
 
-    this.innerHTML = `
+        this.innerHTML = `
         <header id="banner">
           <h1>The Great Outdoors: A Poul Nichols Experience</h1>
       </header>
       `;
-  }
+    }
 }
 
 class NavBar extends HTMLElement {
-  constructor() {
-    super();
-  }
+    constructor() {
+        super();
+    }
 
-  connectedCallback() {
-    this.setAttribute("style", "display: contents;");
+    connectedCallback() {
+        this.setAttribute("style", "display: contents;");
 
-    this.innerHTML = `
+        this.innerHTML = `
          <nav id="navbar">
         <a href=""><img id="logo" src="images/logo.png" alt="Great Outdoors" /></a>
         <ul>
@@ -41,29 +41,29 @@ class NavBar extends HTMLElement {
         </ul>
       </nav>
       `;
-  }
+    }
 }
 
 class RecentPosts extends HTMLElement {
-  constructor() {
-    super();
-  }
+    constructor() {
+        super();
+    }
 
-  connectedCallback() {
-    this.setAttribute("style", "display: contents;");
+    connectedCallback() {
+        this.setAttribute("style", "display: contents;");
 
-    this.innerHTML = `
+        this.innerHTML = `
     <nav id="recent" class="cell">
             <h3>Recent Posts</h3>
     <ul>
         <li><a href="./lake.html">Trip Report: Lake 22</a><br>February 13, , 2025</li>
-        <li><a href="./snowshoe.html">Quieter spots for snowshoeing close to home</a><br> January 27, 2025</li>
+        <li><a href="">Quieter spots for snowshoeing close to home</a><br> January 27, 2025</li>
         <li><a href="./forest.html">Trip Report: Deer Canyon</a><br> December 19, 2025</li>
-        <li><a href="./daypack.html">My new favorite daypack</a><br> November 30, 2025</li>
+        <li><a href="">My new favorite daypack</a><br> November 30, 2025</li>
     </ul>
     </nav>
       `;
-  }
+    }
 }
 
 customElements.define("banner-component", Banner);
